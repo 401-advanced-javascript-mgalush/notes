894e0cf12df9d3a4eb1604f13ae1e570cfcbbb02
Melissa Galush <galushmn@gmail.com>
2020-06-08 21:54:23 -0700

    getting user input from CLI

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const argv = require('minimist')(process.argv.slice(2));
+const Input = require('./lib/input');
+const Notes = require('./lib/notes');
 
-console.log(argv);
+const input = new Input();
+
+const notes = new Notes();
+
+notes.execute(input.note);
